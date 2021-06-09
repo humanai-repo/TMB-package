f9bfa59fe606dfe41dd671341defb670b29f4842
Simon Overell <soverell@gmail.com>
2021-06-09 10:40:26 +0100

    Improved logging and earlyexit
    Add an early exit parameter. If an 8th parameter is included the script will exit after the first file load.

--- R/CalculateTMB.R
+++ R/CalculateTMB.R
@@ -1,20 +1,27 @@
 library(ecTMB)
+library(futile.logger)
 
+flog.threshold(DEBUG)
+
+flog.debug("Read Args")
 args = commandArgs(trailingOnly=TRUE)
-if (length(args)!=7) {
-  stop("Expected Ags: ucec.rda hg38ExomeFile geneProperties mutationContextsTxt mutationtContextsBed grch38.d1 ouput.png.n", call.=FALSE)
+if (length(args)<7) {
+  stop("Expected Ags: ucec.rda hg38ExomeFile geneProperties mutationContextsTxt mutationtContextsBed grch38.d1 ouput.pdf ?earlyexit", call.=FALSE)
 }
 
+flog.debug("Load ucec.rda")
 load(args[1])
 exomef                 = args[2]  #### hg38 exome file
 covarf                 = args[3]   ### gene properties
 mutContextf            = args[4]  ### 96 mutation contexts
 TST170_panel           = args[5]  ### 96 mutation contexts
 ref                    = args[6]
-output 				   = args[7]
+output 				         = args[7]
+
+earlyexit <- length(args) == 8 # An extra optional command has been passed indicating early exit.
 
 
-
+flog.debug("Split test and training samples")
 set.seed(1002200)
 SampleID_all   = UCEC_cli$sample
 SampleID_train = sample(SampleID_all, size = round(2 * length(SampleID_all)/3), replace = F)
@@ -23,14 +30,25 @@ SampleID_test  = SampleID_all[!SampleID_all %in% SampleID_train]
 ## test data for panel TST 170
 sample         = data.frame(SampleID = SampleID_test, BED = TST170_panel, stringsAsFactors = FALSE)
 testData       = UCEC_mafs[UCEC_mafs$Tumor_Sample_Barcode %in% as.character(SampleID_test),]
+
+if(earlyexit) {
+  # Memory usage test. By this point the script will be using ~2G of memory. To test is mem usage 
+  # Write an output and quite.
+  write(length(SampleID_all), output)
+  q()
+}
+
+flog.debug("Read testdata Panel")
 testset_panel  = readData(testData, exomef, covarf, mutContextf, ref, samplef = sample)
+flog.debug("Read Whole Exome Sequence")
 testset_WES    = readData(testData, exomef, covarf, mutContextf, ref)  ## to calculate WES-TMB for test samples
 
-
+flog.debug("Calculate TMB")
 ## process time less than 1s. 
 TMBs          =  pred_TMB(testset_panel, WES = testset_WES, cores = 1,
                         params = trainedModel, mut.nonsil = T)
-                        
+
+flog.debug("Output graph")                        
 ## plot the prediction.    
 library(dplyr)
 library(ggplot2)
