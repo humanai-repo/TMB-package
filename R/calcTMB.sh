c06b916cae3dfc5de23807b5ebf5019e4e0effb7
Simon Overell <soverell@gmail.com>
2021-06-16 14:52:14 +0100

    Change logging and refactor if statements

--- R/calcTMB.sh
+++ R/calcTMB.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
+#
+# Wrapper to run `CalculateTMB.R` in test, train or help mode.
+
+set -ex
 
 : ${INPUT_PATH:="/parcel/data/in"}
 : ${OUTPUT_PATH:="/parcel/data/out"}
-CMD="Rscript /app/CalculateTMB.R --ucec=$INPUT_PATH/UCEC.rda --exomef=$INPUT_PATH/exome_hg38_vep.Rdata --covarf=$INPUT_PATH/gene.covar.txt --mutContextf=$INPUT_PATH/mutation_context_96.txt --TST170_panel=$INPUT_PATH/TST170_DNA_targets_hg38.bed --ref=$INPUT_PATH/GRCh38.d1.vd1.fa --output=$OUTPUT_PATH/tmb.pdf"
+CMD="Rscript /app/CalculateTMB.R --ucec=$INPUT_PATH/UCEC.rda --exomef=$INPUT_PATH/exome_hg38_vep.Rdata --covarf=$INPUT_PATH/gene.covar.txt --mut_contextf=$INPUT_PATH/mutation_context_96.txt --tst_170_panel=$INPUT_PATH/TST170_DNA_targets_hg38.bed --ref=$INPUT_PATH/GRCh38.d1.vd1.fa --output=$OUTPUT_PATH/tmb.pdf"
 
-if [[ $# -eq 1 ]]
-then
-  if [[ "{$1}" = "test" ]]
-  then
-    echo $CMD
-    $CMD
-  elif [[ "{$1}" = "train" ]]
-  then
-    CMD="{$CMD} --train"
-    echo $CMD
-    $CMD
-  elif [[ "{$1}" = "helloworld" ]]
-  then
-    CMD="Rscript /app/CalculateTMB.R --help | tee $OUTPUT_PATH/help.txt"
-    echo $CMD
-    $CMD
-  else
-    echo "Unrecognised arg '{$1}'"
-  fi
-else
+if [[ $# -ne 1 ]]; then
   echo "Usage: /calcTMB.sh (test|train|helloworld)"
+  exit 1
+fi
+
+if [[ "$1" = "test" ]]
+then
+  $CMD
+elif [[ "$1" = "train" ]]
+then
+  CMD="$CMD --train"
+  $CMD
+elif [[ "$1" = "helloworld" ]]
+then
+  Rscript /app/CalculateTMB.R --help | tee $OUTPUT_PATH/help.txt
+else
+  echo "Unrecognised arg '$1'"
 fi
