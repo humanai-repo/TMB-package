98a62463dbaac556fbf049fa3e886b3877397b94
Simon Overell <soverell@gmail.com>
2021-06-17 14:42:45 +0100

    Apply Auto Format for R files

--- R/CalculateTMB.R
+++ R/CalculateTMB.R
@@ -10,28 +10,37 @@ library(reshape)
 parser <- OptionParser()
 parser <- add_option(parser, c("--ucec"), help = "Path to input UCEC.rda")
 parser <- add_option(parser, c("--exomef"),
-  help = "Path to input exome_hg38_vep.Rdata")
+  help = "Path to input exome_hg38_vep.Rdata"
+)
 parser <- add_option(parser, c("--covarf"),
-  help = "Path to input gene.covar.txt")
+  help = "Path to input gene.covar.txt"
+)
 parser <- add_option(parser, c("--mut_contextf"),
-  help = "Path to input mutation_context_96.txt")
+  help = "Path to input mutation_context_96.txt"
+)
 parser <- add_option(parser, c("--tst_170_panel"),
-  help = "Path to input TST170_DNA_targets_hg38.bed")
+  help = "Path to input TST170_DNA_targets_hg38.bed"
+)
 parser <- add_option(parser, c("--ref"),
-  help = "Path to input GRCh38.d1.vd1.fa")
+  help = "Path to input GRCh38.d1.vd1.fa"
+)
 parser <- add_option(parser, c("--output"), help = "Path to output tmb.pdf")
 parser <- add_option(parser, c("--earlyexit"),
   help = "Exit early to test initial processing",
-  action = "store_true", default = FALSE)
+  action = "store_true", default = FALSE
+)
 parser <- add_option(parser, c("--train"),
   help = "Running training in addition to testing",
-  action = "store_true", default = FALSE)
-parser <- add_option(parser, c("--quiet"), help = "Quiet output",
-  action = "store_true", default = FALSE)
+  action = "store_true", default = FALSE
+)
+parser <- add_option(parser, c("--quiet"),
+  help = "Quiet output",
+  action = "store_true", default = FALSE
+)
 
 args <- parse_args(parser)
 
-if (! args$quiet) {
+if (!args$quiet) {
   flog.threshold(DEBUG)
 }
 
@@ -43,13 +52,16 @@ set.seed(1002200)
 sample_id_all <- UCEC_cli$sample
 sample_id_train <-
   sample(sample_id_all,
-    size = round(2 * length(sample_id_all) / 3), replace = F)
+    size = round(2 * length(sample_id_all) / 3), replace = F
+  )
 sample_id_test <- sample_id_all[!sample_id_all %in% sample_id_train]
 
 # test data for panel TST 170
 sample <-
-  data.frame(SampleID = sample_id_test,
-    BED = args$tst_170_panel, stringsAsFactors = FALSE)
+  data.frame(
+    SampleID = sample_id_test,
+    BED = args$tst_170_panel, stringsAsFactors = FALSE
+  )
 test_data <-
   UCEC_mafs[UCEC_mafs$Tumor_Sample_Barcode %in% as.character(sample_id_test), ]
 
@@ -64,17 +76,21 @@ if (args$earlyexit) {
 flog.debug("Read testdata Panel")
 testset_panel <-
   readData(test_data, args$exomef, args$covarf, args$mut_contextf, args$ref,
-    samplef = sample)
+    samplef = sample
+  )
 flog.debug("Read Whole Exome Sequence")
 # Use to calculate WES-TMB for test samples
-testset_wes <- readData(test_data, args$exomef, args$covarf, args$mut_contextf,
-  args$ref)
+testset_wes <- readData(
+  test_data, args$exomef, args$covarf, args$mut_contextf,
+  args$ref
+)
 
 if (args$train) {
   flog.debug("Training samples")
   train_data <-
     UCEC_mafs[
-      UCEC_mafs$Tumor_Sample_Barcode %in% as.character(SampleID_train), ]
+      UCEC_mafs$Tumor_Sample_Barcode %in% as.character(SampleID_train),
+    ]
   trainset <-
     readData(train_data, args$exomef, args$covarf, args$mutContextf, args$ref)
 
@@ -85,28 +101,37 @@ if (args$train) {
 
 flog.debug("Calculate TMB")
 # process time less than 1s.
-tmbs <- pred_TMB(testset_panel, WES = testset_wes, cores = 1,
-  params = trainedModel, mut.nonsil = T)
+tmbs <- pred_TMB(testset_panel,
+  WES = testset_wes, cores = 1,
+  params = trainedModel, mut.nonsil = T
+)
 
 flog.debug("Output graph")
 # plot the prediction.
 p <- tmbs %>%
   melt(id.vars = c("sample", "WES_TMB")) %>%
   ggplot(
-    aes(x = WES_TMB, y = value,
+    aes(
+      x = WES_TMB, y = value,
       color =
-        factor(variable, levels = c("ecTMB_panel_TMB",  "count_panel_TMB")),
-      group = factor(variable))) +
+        factor(variable, levels = c("ecTMB_panel_TMB", "count_panel_TMB")),
+      group = factor(variable)
+    )
+  ) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0) +
   scale_x_continuous(trans = "log2") +
   scale_y_continuous(trans = "log2") +
   theme_bw() +
-  theme(panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank()) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  ) +
   theme(legend.title = element_blank()) +
-  labs(x = "TMB defined by WES",
-    y = sprintf("Predicted TMB from panel: TST170"))
+  labs(
+    x = "TMB defined by WES",
+    y = sprintf("Predicted TMB from panel: TST170")
+  )
 
 pdf(args$output)
 print(p)
